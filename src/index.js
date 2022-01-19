bef1cd58c03c67caa07e46366bcf621f523487a2
Kirill Loiko <kirill.loiko@gmail.com>
2022-01-19 16:45:39 +0300

    feat: new function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -n;
+    }
+
+    let numberToString = String(n);
+    let i = numberToString.length - 1
+    let result = '';
+
+    while (i >= 0) {
+        result = result + numberToString[i];
+        i--;
+    }
+    return +result;
 }
